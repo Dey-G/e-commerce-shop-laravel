4070dac23bce6cfe51c1717fdaa4c472a4049606
Dey'G <galeriodexter@gmail.com>
2026-05-19 21:13:31 +0800

    Added Laravel API delete endpoint for products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -179,9 +179,20 @@ class ProductController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Product $product)
-    {
-        $product->delete();
-        return redirect("/admin");
+    public function destroy($id)
+{
+    $product = Product::find($id);
+
+    if (!$product) {
+        return response()->json([
+            'message' => 'Product not found'
+        ], 404);
     }
+
+    $product->delete();
+
+    return response()->json([
+        'message' => 'Product deleted successfully'
+    ]);
+}
 }
