8ba3e0888522a4efc06e6ce6d0c6354c7977829b
Dey'G <galeriodexter@gmail.com>
2026-05-20 12:14:13 +0800

    Added customer checkout system and my orders page

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -7,9 +7,12 @@
         </a>
 
         <!-- MOBILE BUTTON -->
-        <button class="navbar-toggler border-0 shadow-none" type="button" data-bs-toggle="collapse"
+        <button class="navbar-toggler border-0 shadow-none" type="button"
+            data-bs-toggle="collapse"
             data-bs-target="#mainNavbar">
+
             <span class="navbar-toggler-icon"></span>
+
         </button>
 
         <!-- NAVBAR -->
@@ -19,29 +22,57 @@
             <ul class="navbar-nav mx-auto mb-2 mb-lg-0 gap-lg-3">
 
                 <li class="nav-item">
-                    <a class="nav-link text-white fw-semibold" href="{{ route('home.index') }}">
-                        <i class="fa-solid fa-house me-1"></i> Home
+                    <a class="nav-link text-white fw-semibold"
+                        href="{{ route('home.index') }}">
+
+                        <i class="fa-solid fa-house me-1"></i>
+                        Home
+
                     </a>
                 </li>
 
                 <li class="nav-item">
-                    <a class="nav-link text-white fw-semibold" href="{{ route('products.index') }}">
-                        <i class="fa-solid fa-store me-1"></i> Products
+                    <a class="nav-link text-white fw-semibold"
+                        href="{{ route('products.index') }}">
+
+                        <i class="fa-solid fa-store me-1"></i>
+                        Products
+
                     </a>
                 </li>
 
                 <li class="nav-item">
-                    <a class="nav-link text-white fw-semibold" href="{{ route('home.about') }}">
-                        <i class="fa-solid fa-circle-info me-1"></i> About
+                    <a class="nav-link text-white fw-semibold"
+                        href="{{ route('home.about') }}">
+
+                        <i class="fa-solid fa-circle-info me-1"></i>
+                        About
+
                     </a>
                 </li>
 
                 @auth
+
                 <li class="nav-item">
-                    <a class="nav-link text-white fw-semibold" href="{{ route('cart.index') }}">
-                        <i class="fa-solid fa-cart-shopping me-1"></i> Cart
+                    <a class="nav-link text-white fw-semibold"
+                        href="{{ route('cart.index') }}">
+
+                        <i class="fa-solid fa-cart-shopping me-1"></i>
+                        Cart
+
                     </a>
                 </li>
+
+                <li class="nav-item">
+                    <a class="nav-link text-white fw-semibold"
+                        href="{{ route('orders.index') }}">
+
+                        <i class="fa-solid fa-bag-shopping me-1"></i>
+                        My Orders
+
+                    </a>
+                </li>
+
                 @endauth
 
             </ul>
@@ -51,12 +82,18 @@
 
                 @guest
 
-                    <a href="{{ route('login') }}" class="btn btn-outline-light px-4">
+                    <a href="{{ route('login') }}"
+                        class="btn btn-outline-light px-4">
+
                         Login
+
                     </a>
 
-                    <a href="{{ route('register') }}" class="btn btn-warning fw-semibold px-4">
+                    <a href="{{ route('register') }}"
+                        class="btn btn-warning fw-semibold px-4">
+
                         Register
+
                     </a>
 
                 @endguest
@@ -89,7 +126,9 @@
                             </li>
 
                             <li>
-                                <form method="POST" action="{{ route('logout') }}">
+                                <form method="POST"
+                                    action="{{ route('logout') }}">
+
                                     @csrf
 
                                     <button type="submit"
@@ -98,6 +137,7 @@
                                         Logout
 
                                     </button>
+
                                 </form>
                             </li>
 
